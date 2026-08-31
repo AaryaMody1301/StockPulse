ca8a8529dc77de09682f32f3c9eebcb124981c7a
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-08-31 08:31:18 +0530

    chore: remove unused provider limit constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,16 +7,6 @@ export const US_MARKET = {
   timezone: "America/New_York",
 } as const;
 
-/** Finnhub free-tier rate limit: 60 calls/minute */
-export const FINNHUB_RATE_LIMIT = 60;
-
-/** Twelve Data free-tier rate limit: 800 calls/day, 8/minute */
-export const TWELVEDATA_RATE_LIMIT_PER_MINUTE = 8;
-export const TWELVEDATA_RATE_LIMIT_PER_DAY = 800;
-
-/** Default number of symbols displayed on the homepage */
-export const DEFAULT_PAGE_SIZE = 20;
-
 /** Cache revalidation intervals (seconds) */
 export const REVALIDATE = {
   quotes: 15,
